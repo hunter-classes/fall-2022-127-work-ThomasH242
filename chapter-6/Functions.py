4fcafd83168c3c0cc4e62aa0ee802dd05abcb94c
ThomasH242 <thomas.huang45@myhunter.cuny.edu>
2022-09-08 09:10:19 -0400

    fixed

---
+++ chapter-6/Functions.py
@@ -0,0 +1,19 @@
+import turtle
+
+def square(t):
+    for i in range(4):
+        t.forward(50)
+        t.right(90)
+wn = turtle.Screen()
+crush = turtle.Turtle()
+square(crush)
+squirt = turtle.Turtle()
+squirt.penup()
+squirt.goto(100,100)
+squirt.pendown()
+squirt.color("red")
+squirt.width(5)
+square(squirt)
+
+wn.exitonclick()
+wn.mainloop()
