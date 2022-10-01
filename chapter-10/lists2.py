b4c5c2d155fbd95dd6193cfef7097a9ac85ecb15
Thomas H <Thomas.huang45@myhunter.cuny.edu>
2022-10-01 17:59:23 -0400

    hw

--- chapter-10/lists2.py
+++ chapter-10/lists2.py
@@ -2,8 +2,15 @@
 
 def average(n):
     num = 0
-    for i in len(n):
-        num = n.pop(i)
-    return num/len(n)
-test = [1,2,3,4,5]
-average(test)
+    for i in n: # goes to the next int of the list
+        num += i# 'i' gets the number from the list and adds it to num
+    return num/len(n) # divide by size of list
+test = [1,2,5,5,5]
+print(average(test))
+
+def sum_of_squares(xs):
+    num = 0
+    for i in xs:# goes to the next int of the list
+        num += i**2 # i^2 adds to num
+    return num
+print(sum_of_squares(test))
