428f7c8a22cf2a42f3dadaeaa8d22c4fb8081a69
Thomas H <Thomas.huang45@myhunter.cuny.edu>
2022-10-17 23:44:01 -0400

    comments

--- project-01-madlibs/madlibs.py
+++ project-01-madlibs/madlibs.py
@@ -14,33 +14,35 @@ hread = H.read()
 vlist = vread.split()
 nlist = nread.split()
 hlist = hread.split()
-print(vlist,hlist,nlist)
 
 #functions
 def filters(S): 
     NS = S.split() #New Sentence
     c = 0 # int index
-    v = ''
-    n = ''
     for i in S.split():
 #verb
         if i.lower() == '<verb>':
-            ran = random.randint(0,len(vlist)-1)
+    # gets a random number from 0: length of verb list-1(it counts the size of the list STARTING at 1)
+            ran = random.randint(0,len(vlist)-1) 
+    # the new list[int index] = a verblist[random].lowercase()
             NS[c] = vlist[ran].lower()
-            # If its the first word
+    # If its the first word or the previous word had a period
             if(c == 0 or NS[c-1].find('.') > 0):
                 NS[c] = vlist[ran].capitalize()
-                print("lets go")
 #noun
         elif i.lower() == '<noun>':
+    # gets a random number from 0: length of noun list-1
             ran = random.randint(0,len(nlist)-1)
+    # new list[int index] = a nounlist[random].lowercase()
             NS[c] = nlist[ran].lower()
-            #if its the first word 
+    # first word or the previous word had a period
             if(c == 0 or NS[c-1].find('.') > 0):
                 NS[c] = nlist[ran].capitalize()
 #Hero 
         elif i.lower() == '<hero>':
+    # random number from 0: length of hero list-1
             ran = random.randint(0,len(hlist)-1)
+    # new list[int index] = a herolist[random]
             NS[c] = hlist[ran].capitalize()
         c+=1
     return " ".join(NS)            
