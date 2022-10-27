749cb0f647e57886cde036fd3fda179037cae93a
ThomasH242 <Thomas.Huang45@myhunter.cuny.edu>
2022-10-27 09:39:21 -0400

    mode

--- mode/modes.py
+++ mode/modes.py
@@ -1,4 +1,4 @@
-
+import random
 def findLargest(l):
     max = l[0]
     for i in l:
@@ -12,7 +12,30 @@ def freq(l,v):
         if i == v:
             c+=1
     return c
+def mode(l):
+    msf = l[0]
+    freqsf = freq(l,msf)
+    for i in l:
+        if freq(l,i) > freqsf:
+            msf = i
+            freqsf = freq(l,i)
+    return msf
 
-list = (4,2,1,5,8,4,4,1,7,2,6)
+def buildRandomList(s,m):#size/max value
+    l = []
+    i = 0
+    while i != s:
+        l.append(random.randint(1,m))
+        i+=1
+    return l
+def testMode(s,max):
+    l = buildRandomList(s,max)
+    m = mode(l)
+    print(m)
+    return m
+list = (4,2,1,5,8,4,4,1,7,2,6,8,8,8)
 print(findLargest(list), "find largest")
 print("there are ",freq(list,4), "of the same numbers")
+print(mode(list))
+print(buildRandomList(10,20))
+print(testMode(20,10))
